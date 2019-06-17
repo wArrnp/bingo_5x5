9a2025b8e00b7c193b829d55d29655ddc8f5b3f7
gtobio11 <gtobio11@gmail.com>
2019-06-17 19:41:17 +0900

    [MODIFY] variable naming clearly, [REMOVE] unusable code

--- src/reducers/bingo.js
+++ src/reducers/bingo.js
@@ -22,12 +22,12 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case SET_GAME:
-      const newFstBoard = action.fstBoard.map(d => ({
-        number: d,
+      const newFstBoard = action.fstBoard.map(number => ({
+        number,
         picked: false
       }));
-      const newSndBoard = action.sndBoard.map(d => ({
-        number: d,
+      const newSndBoard = action.sndBoard.map(number => ({
+        number,
         picked: false
       }));
 
@@ -44,18 +44,16 @@ export default function(state = initialState, action) {
     case CLICK_CELL:
       const fstBoard = [...state.fstBoard];
       const sndBoard = [...state.sndBoard];
-      const fstIndex = fstBoard.findIndex(d => d.number === action.number);
-      const sndIndex = sndBoard.findIndex(d => d.number === action.number);
+      const fstIndex = fstBoard.findIndex(
+        item => item.number === action.number
+      );
+      const sndIndex = sndBoard.findIndex(
+        item => item.number === action.number
+      );
       fstBoard[fstIndex].picked = true;
       sndBoard[sndIndex].picked = true;
-      const fstBingoList = checkBingo(
-        fstBoard,
-        fstBoard.findIndex(d => d.number === action.number)
-      );
-      const sndBingoList = checkBingo(
-        sndBoard,
-        sndBoard.findIndex(d => d.number === action.number)
-      );
+      const fstBingoList = checkBingo(fstBoard, fstIndex);
+      const sndBingoList = checkBingo(sndBoard, sndIndex);
 
       return {
         ...state,
